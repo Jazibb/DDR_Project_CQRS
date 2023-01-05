5cbde9d54aba780ee7bd54a5fc3a97c6b1b8af3d
Magician07 <78610174+Magician07@users.noreply.github.com>
2023-01-06 00:48:47 +0500

    full commit

--- cqrs_project/src/main/java/com/example/cqrs_project/query/service/ManageAccountService.java
+++ cqrs_project/src/main/java/com/example/cqrs_project/query/service/ManageAccountService.java
@@ -1,9 +1,14 @@
 package com.example.cqrs_project.query.service;
+import com.example.cqrs_project.common.event.AccountActivatedEvent;
+import com.example.cqrs_project.common.event.AccountCreditedEvent;
+import com.example.cqrs_project.common.event.AccountDebitedEvent;
+import com.example.cqrs_project.query.query.FindAccountByIdQuery;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.eventhandling.EventHandler;
 import com.example.cqrs_project.common.event.AccountCreatedEvent;
 import com.example.cqrs_project.query.entity.Account;
 import com.example.cqrs_project.query.repository.AccountRepository;
+import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,6 +31,47 @@ public class ManageAccountService {
         account.setStatus("CREATED");
 
         accountRepository.save(account);
+    }
+    @EventHandler
+    public void on(AccountActivatedEvent accountActivatedEvent) {
+        log.info("Handling AccountActivatedEvent...");
+        Account account = accountRepository.findById(accountActivatedEvent.getId()).orElse(null);
+
+        if (account != null) {
+            account.setStatus(accountActivatedEvent.getStatus());
+            accountRepository.save(account);
+        }
+    }
+    @EventHandler
+    public void on(AccountCreditedEvent accountCreditedEvent) {
+        log.info("Handling AccountCreditedEvent...");
+        Account account = accountRepository
+                .findById(accountCreditedEvent.getId()).orElse(null);
+
+        if (account != null) {
+            account.setBalance(account.getBalance()
+                    .add(accountCreditedEvent.getAmount()));
+        }
+    }
+    @EventHandler
+    public void on(AccountDebitedEvent accountDebitedEvent) {
+        log.info("Handling AccountDebitedEvent...");
+        Account account = accountRepository
+                .findById(accountDebitedEvent.getId()).orElse(null);
+
+        if (account != null) {
+            account.setBalance(account.getBalance()
+                    .subtract(accountDebitedEvent.getAmount()));
+        }
+    }
+
+    @QueryHandler
+    public Account handle(FindAccountByIdQuery query) {
+        log.info("Handling FindAccountByIdQuery...");
+        Account account = accountRepository
+                .findById(query.getAccountId()).orElse(null);
+
+        return account;
     }
 
 }
